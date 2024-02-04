a3bf13474a869249ba063edd917283db15593239
Jo Martin Slåtten <jomartinslaatten98@hotmail.com>
2024-02-04 12:08:34 +0100

    Jobber videre på lecture3

--- GPS_greier/lecture3.py
+++ GPS_greier/lecture3.py
@@ -2,6 +2,7 @@
 
 class GPSPoint:
     def __init__(self, time, lat, long , height):
+        print("Dette er en konstuktør! :D")
         self.time = time
         self.lat = lat
         self.long = long
@@ -19,4 +20,13 @@ point = GPSPoint(
     lat = 60.376988, 
     long = 5.227082, 
     height = 105.5)
-
+print(id(point))
+print(type(point))
+print(dir(point))
+print(point)
+print("--------")
+print("er lat i denne? ", "lat" in dir(point))
+print("food" in dir(point))
+print(point.height)
+point.foord = "Snikkers"
+print(point.foord)
