e90bb40ce33fa42e32bfb5f04240f288499a1e3e
Jo Martin Slåtten <jomartinslaatten98@hotmail.com>
2024-02-06 14:02:47 +0100

    lagt til skrivefunksjon i lecture3

--- lecture3.py
+++ lecture3.py
@@ -1,5 +1,11 @@
 
-f = open("demofile2.TcPOU", "a")
+
+
+
+
+
+
+filepath = r"C:\Users\jomar\OneDrive\Dokumenter\TcXaeShell\hello world\hello world\HelloWorldPLC\POUs\testPOU.TcPOU"
+f = open(filepath, "a")
 f.write("Now the file has more content!")
-
-
+f.close()
